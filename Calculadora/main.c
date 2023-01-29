a4c607519cc9ef7cbbff91e0eec912bc67c36551
pdrsix <pedro-psc@hotmail.com>
2023-01-28 22:24:55 -0300

    Create main.c

---
+++ Calculadora/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    int i;
+    float num1, num2, soma, sub, div, mult;
+    printf("\t\tCalculadora Simples em C\t\t\n");
+
+    do{
+    printf("Escolha entre: \n1 - soma \n2 - subtração \n3 - divisão \n4 - multiplicação\n");
+    scanf("%d", &i);
+    }while(i>4 || i<1);
+    printf("Digite os dois numeros para o calculo:\n");
+    scanf("%f %f", &num1, &num2);
+
+    switch(i){
+        case 1:
+            soma = num1 + num2;
+            printf("O resultado da soma de %.2f + %.2f = %.2f", num1, num2, soma);
+            break;
+        case 2:
+            sub = num1 - num2;
+            printf("O resultado da subtração de %.2f - %.2f = %.2f", num1, num2, sub);
+            break;
+        case 3:
+            div = num1 / num2;
+            printf("O resultado da divisão de %.2f / %.2f = %.2f", num1, num2, div);
+            break;
+        case 4:
+            mult = num1 * num2;
+            printf("O resultado da multiplicação de %.2f * %.2f = %.2f", num1, num2, mult);
+            break;
+        default:
+            printf("Numero invalido");
+        }
+
+
+    return 0;
+}
